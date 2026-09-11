b28ccc6d2532bfa92362e467db6bacd8fc970829
Krishna Chauhan <krishnachauhaniitb2023@gmail.com>
2026-09-11 12:12:16 +0530

    Add Тройные операции (1300)

---
+++ codeforces/1300/1999E - Тройные операции.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+const ll N = 2e5 + 5;
+ll pre[N];
+
+ll cnt3(ll n)
+{
+    ll cnt = 0;
+
+    while (n > 0)
+    {
+        n /= 3;
+        cnt++;
+    }
+
+    return cnt;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    for (ll i = 1; i < N; i++)
+    {
+        pre[i] = pre[i - 1] + cnt3(i);
+    }
+
+    ll t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll l, r;
+        cin >> l >> r;
+
+        ll ans = cnt3(l) + pre[r] - pre[l - 1];
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
